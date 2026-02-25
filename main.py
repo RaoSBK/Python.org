08831811d3bc062e237d0705cce1fa5deced3a24
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-02-26 00:08:55 +0530

    Compliting unit-2

---
+++ main.py
@@ -0,0 +1,63 @@
+print ("Hello wrodl");
+
+
+massage = "hello suraj"
+print(massage)
+
+massage = "hello there how are you"
+print(massage)
+
+
+name = 'my name is suraj bhan'
+print(name);
+print(type(name))
+u_name = (name.upper())
+print(u_name)
+l_name = u_name.lower()
+print(l_name)
+full_name = f"{u_name} {l_name}"
+print(full_name)
+
+print(f"{full_name.title()}") #for doing the capital of every first latter of the particular word in an entire sentence 
+
+massage = f"Hello, {full_name.title()}"
+print(massage)
+
+full_name = "{} {}".format(u_name, l_name) #help us to print lower and upper case at once 
+print(full_name)
+
+print("\tSurajBhan")        #\t used for giving the tab line
+
+fav_lang = " PyThon  "
+print(fav_lang.rstrip())
+print(fav_lang)
+print(fav_lang.lstrip())
+print(fav_lang.strip())
+
+# numbers
+print(2**5)     # power
+print(5+7.46)
+universe_age = 14_3475_000
+print(universe_age)
+
+x,y,z = 4,5,7
+print(x)
+print(y)
+print(z)
+
+
+# problem no 2-8
+
+print(5+3)
+print(10-2)
+print(4*2)
+print(16/2)
+
+
+#problem no 2-9
+
+fav_num = 777
+print(fav_num)
+
+fav_num = "This variable contains my favourite number "
+print(fav_num)
