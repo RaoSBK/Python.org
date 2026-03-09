01d180645dca1b79ae1b3b11184b5e9f24f5c091
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-03-09 23:23:31 +0530

    In between of unit 3

--- unit3.py
+++ unit3.py
@@ -59,3 +59,74 @@ first_owned = motorcycles.pop(0)
 print(f"I owned my first bike which is {first_owned.title()}")
 
 
+print(f"My first motorcycle is {first_owned.title()} ")
+
+print(motorcycles)
+
+
+too_expensive = 'bugati'
+
+print(f"{too_expensive.title()} is too expensive bike")
+
+
+#3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who 
+#would you invite? Make a list that includes at least three people you’d like to 
+#invite to dinner. Then use your list to print a message to each person, inviting 
+#them to dinner.
+
+
+guests = ['swaraj', 'bamshi', 'sourabh']
+invitation = "i am requestig you to come to the dinner at 9 O clock"
+print(invitation, {guests[0].title()})
+print(invitation, guests[1].title())
+print(invitation, guests[-1].title())
+
+for guest in guests:
+    print(guest.title(), invitation )
+
+
+# Q 3-5
+
+# removed_guest = guests.pop(0)
+# print(f"Guys {removed_guest} is  not comming ")
+guests[0] = "suraj"
+for guest in guests:
+    print(guest.title(), invitation)
+
+
+print("Q: 3-6")
+guests.insert(0,'Shubham')
+
+guests.insert(len(guest)//2, 'Biswas')
+
+guests.append('Shiavm')
+
+for guest in guests:
+    print(guest.title(), invitation)
+
+
+print("Q: 3-7")
+print(guests)
+print("Sorry guys the new table is not reached yet so only two peoples are invited")
+
+removed_guests = guests.pop(0)
+sorry_massage = ", I'm really sorry for that but for some issue I'm not going to invite you "
+print(removed_guests.title(), invitation)
+
+
+removed_guests = guests.pop(1)
+print(removed_guests.title(), invitation)
+
+removed_guests = guests.pop(2)
+print(removed_guests.title(), invitation)
+
+removed_guests = guests.pop(3)
+print(removed_guests.title(), invitation)
+
+removed_guests = guests.pop(4)
+print(removed_guests.title(), invitation)
+
+
+for guest in guests:
+    print(guests.title(), invitation)
+
