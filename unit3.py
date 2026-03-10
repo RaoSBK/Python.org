b2258c0dffc5741645deb609c56308c72990cda6
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-03-10 23:34:50 +0530

    Doing the excerices of the list

--- unit3.py
+++ unit3.py
@@ -120,13 +120,105 @@ print(removed_guests.title(), invitation)
 removed_guests = guests.pop(2)
 print(removed_guests.title(), invitation)
 
-removed_guests = guests.pop(3)
-print(removed_guests.title(), invitation)
+# removed_guests = guests.pop(3)
+# print(removed_guests.title(), invitation)
 
-removed_guests = guests.pop(4)
-print(removed_guests.title(), invitation)
+# removed_guests = guests.pop(4)
+# print(removed_guests.title(), invitation)
 
 
 for guest in guests:
-    print(guests.title(), invitation)
+    print(guest.title(), invitation)
 
+
+print(guests)
+
+
+del guests[0]
+print(guests)
+
+del guests[0]
+print(guests)
+
+del guests[0]
+print(guests)
+
+
+
+cars = ['bmw', 'urus', 'lambo', 'range rover', 'toyoto']
+cars.sort()
+print(cars)
+
+print('Here is the original list of the cars')
+print(cars)
+
+cars.sort(reverse= True)
+print(cars)
+
+print('Here is the original list of the cars')
+print(cars)
+
+
+print('Here is the sorted cars list')
+print(sorted(cars))
+
+print("here is the original  list again of these cars")
+print(cars)
+
+cars = ['bmw', 'urus', 'lambo', 'range rover', 'toyoto']
+cars.reverse()
+print(cars)
+
+
+print(len(cars))
+
+
+# 3-8. Seeing the World: Think of at least five places in the world you’d like to 
+# visit.
+# •	Store the locations in a list. Make sure the list is not in alphabetical order.
+# •	Print your list in its original order. Don’t worry about printing the list neatly, 
+# just print it as a raw Python list.
+# •	Use sorted() to print your list in alphabetical order without modifying the 
+# actual list.
+# •	Show that your list is still in its original order by printing it.
+# •	Use sorted() to print your list in reverse alphabetical order without chang
+# ing the order of the original list.
+# •	Show that your list is still in its original order by printing it again.
+# •	Use reverse() to change the order of your list. Print the list to show that its 
+# order has changed.
+# •	Use reverse() to change the order of your list again. Print the list to show 
+# it’s back to its original order.
+# •	Use sort() to change your list so it’s stored in alphabetical order. Print the 
+# list to show that its order has been changed.
+# •	Use sort() to change your list so it’s stored in reverse alphabetical order. 
+# Print the list to show that its order has changed.
+
+
+
+places = ['Sweetzer land', 'yale', 'New york', 'America', 'berkly']
+print(places)
+
+print(sorted(places))
+
+
+
+print('The original list of the places')
+print(places)
+
+
+print(places.sort(reverse= True))
+
+print('here is again the orignal list again')
+print(places)
+
+places.reverse()
+print(places)
+
+
+places.reverse()
+print(places)
+
+print(sorted(places))
+
+places.sort(reverse= True)
+print(places)
