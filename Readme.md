9edc4e3af17d4311805cff7d0cfe6436a0e454fa
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-03-14 22:53:03 +0530

    Unit 5 is completed (Conditional Statement),and  Readme file is added

---
+++ Readme.md
@@ -0,0 +1,49 @@
+# Python Learning Repository
+
+## 📌 About
+This repository contains my daily Python practice code.  
+Currently, I am in the learning phase and continuously improving my Python skills by writing code every day.
+
+The goal of this repository is:
+- To maintain consistency in coding
+- To track my progress
+- To practice Python concepts step by step
+- To build a strong foundation in programming
+
+I push code daily as part of my learning routine.
+
+---
+
+## 📂 Folder Structure
+Each file in this repository represents practice code related to:
+- Main.py (contains the material of tbe first unit) 
+- Unit-2
+- Unit -3
+- Unit -4
+- Unit -5
+
+More topics will be added as I learn.
+
+---
+
+## ⚙️ Installation Process
+
+Follow these steps to run the code on your system.
+
+### 1. Install Python
+
+Download Python from the official website:
+
+https://www.python.org/downloads/
+
+After installation, check version:
+python --version
+
+---
+
+
+##  Running instructions 
+### 1. open teminal 
+### 2. set the correct path of the terminal 
+### 3. python filename.py 
+### 4. you can view your result 
--- unit5.py
+++ unit5.py
@@ -159,3 +159,327 @@ print('mushrooms' in requested_toppings)
 
 print("Is 'tiika' in requested_toopongs? I predict false")
 print('tikka' in requested_toppings)
+
+
+age = 19
+if age >18:
+    print("You can vote")
+    print("Have you registed to vote yet?")
+else: 
+    print("You're not elgible to vote")
+    print("Please register to vote as soon as you turn 18.")
+
+
+
+age = 20
+if age < 5:
+    print("You are in less than 4  yrs old so your addmision fee is free")
+elif age > 5 and age < 18:
+    print("Your addmision fee is $25")
+elif age >= 18:
+    print("Your addmison fee is $40")
+else:
+    print("You enterd wrong value")
+
+
+age = 12
+if age <  4:
+    price = 0
+elif age >4 and age < 18:
+    price = 25
+elif age > 18:
+    price = 40
+
+print(f"Your admmions cost is ${price}.")
+
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms")
+if 'papperoini' in requested_toppings:
+    print("Adding papperoini")
+if 'extra cheese' in requested_toppings:
+    print("adding extra cheese")
+
+
+
+print("\n Finished making pizza")
+
+
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms")
+elif 'papperoini' in requested_toppings:
+    print("Adding papperoini")
+elif 'extra cheese' in requested_toppings:
+    print("adding extra cheese")
+
+print("\n Finished making pizza")
+
+
+
+# Q 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a 
+# variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# •	Write an if statement to test whether the alien’s color is green. If it is, print 
+# a message that the player just earned 5 points.
+# •	Write one version of this program that passes the if test and another that 
+# fails. (The version that fails will have no output.)
+
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("You got 5 points")
+
+
+
+# Q 5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and 
+# write an if-else chain.
+# •	If the alien’s color is green, print a statement that the player just earned 
+# 5 points for shooting the alien.
+# •	If the alien’s color isn’t green, print a statement that the player just earned 
+# 10 points.
+# •	Write one version of this program that runs the if block and another that 
+# runs the else block.
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You secured 5 points")
+elif alien_color  == 'red':
+    print('You loss 5 points')
+else:
+    print('You secured 10 points')
+
+# Q 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif
+# else chain.
+# •	If the alien is green, print a message that the player earned 5 points.
+# •	If the alien is yellow, print a message that the player earned 10 points.
+# •	If the alien is red, print a message that the player earned 15 points.
+# •	Write three versions of this program, making sure each message is printed 
+# for the appropriate color alien.
+
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You secured 5 points")
+elif alien_color  == 'yellow':
+    print('You secured 10 points')
+elif alien_color == 'red':
+    print("You secured 15 points")
+else:
+    print('You secured 10 points')
+
+
+print("Is alian color is green? I assumed true")
+print(alien_color == 'green')
+
+print("Is alian color is red? I assuned false ")
+print(alien_color == 'red')
+
+
+# Q 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s 
+# stage of life. Set a value for the variable age, and then:
+# •	If the person is less than 2 years old, print a message that the person is 
+# a baby.
+# •	If the person is at least 2 years old but less than 4, print a message that 
+# the person is a toddler.
+# •	If the person is at least 4 years old but less than 13, print a message that 
+# the person is a kid.
+# •	If the person is at least 13 years old but less than 20, print a message that 
+# the person is a teenager.
+# •	If the person is at least 20 years old but less than 65, print a message that 
+# the person is an adult.
+# •	If the person is age 65 or older, print a message that the person is an 
+# elder.
+
+age = 45
+
+if age < 2:
+    print("you are a baby")
+elif age > 2 and age < 4:
+    print("you are a toddler")
+elif age >= 4 and age < 13:
+    print("You are a kid")
+elif age >= 13 and age < 20:
+    print("You are a teenager")
+elif age >= 20 and age < 65:
+    print("Your are an adult")
+elif age >= 65:
+    print("Your are an elder person")
+else:
+    print("You enterd an wrong age")
+
+
+
+# Q 5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of 
+# independent if statements that check for certain fruits in your list.
+# •	Make a list of your three favorite fruits and call it favorite_fruits.
+# •	Write five if statements. Each should check whether a certain kind of fruit 
+# is in your list. If the fruit is in your list, the if block should print a statement, 
+# such as You really like bananas!
+
+fav_fruits = ['banana', 'guava', 'mango']
+if 'banana' in fav_fruits:
+    print("You really like banana")
+if 'guava' in fav_fruits:
+    print("You really like guava")
+if 'mango' in fav_fruits:
+    print("You really like mango")
+else:
+    print("Fruits is not available")
+
+
+requested_toppings =  ['mushrooms', 'green papers', 'extra cheese']
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}")
+
+print("\n Finished making pizza")
+
+
+requested_toppings =  ['mushrooms', 'green pappers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green pappers':
+        print("Sorry green papper is out of stock right now")
+    else:
+        print(f"Adding {requested_topping}")
+
+print("\n Finished making pizza")
+
+
+requested_toppings =  []
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("Finished making your pizza")
+else:
+    print("Are you sure You want a plain pizza")
+
+print("\n")
+
+available_toppings =  ['mushrooms', 'olives', 'green peppers','pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'green pappers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}")
+    else:
+        print(f"Sorry we don't have {requested_topping}")
+
+print("Finished Your making pizza")
+
+# Q 5-8. Hello Admin: Make a list of five or more usernames, including the name 
+# 'admin'. Imagine you are writing code that will print a greeting to each user 
+# after they log in to a website. Loop through the list, and print a greeting to 
+# each user:
+# •	If the username is 'admin', print a special greeting, such as Hello admin, 
+# would you like to see a status report?
+# •	Otherwise, print a generic greeting, such as Hello Jaden, thank you for 
+# logging in again.
+
+users = ['admin', 'michel', 'sophea', 'simen', 'elon' ]
+
+for user in users:
+    if user == 'admin':
+        print("Hello admin, would you like to see a status report")
+    else:
+        print(f"Hello {user}, thank you for logging in again")
+
+    
+
+# Q 5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is 
+# not empty.
+# •	If the list is empty, print the message We need to find some users!
+# •	Remove all of the usernames from your list, and make sure the correct 
+# message is printed
+
+
+users = []
+
+if not users:
+    print("We need to find new users")
+else:
+    for user in users:
+        if user == 'admin':
+            print("Hello admin, would you like to see a status report")
+        else:
+            print(f"Hello {user}, thank you for logging again")
+
+
+# Q 5-10. Checking Usernames: Do the following to create a program that simulates 
+# how websites ensure that everyone has a unique username.
+# •	Make a list of five or more usernames called current_users.
+# •	Make another list of five usernames called new_users. Make sure one or 
+# two of the new usernames are also in the current_users list.
+# •	Loop through the new_users list to see if each new username has already 
+# been used. If it has, print a message that the person will need to enter a 
+# new username. If a username has not been used, print a message saying 
+# that the username is available.
+# •	Make sure your comparison is case insensitive. If 'John' has been used, 
+# 'JOHN' should not be accepted. (To do this, you’ll need to make a copy of 
+# current_users containing the lowercase versions of all existing users.)
+
+
+current_users = ['admin', 'michle', 'saniya', 'elon', 'suraj']
+
+new_users = ['sophea', 'mark', 'charles', 'simon', 'suraj']
+
+current_users_lower = [user.lower() for user in current_users] # this lines helps to transper all the user into lower case
+
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print(f"User name {new_user} is alredy available in the database, You need to enter new username")
+    else:
+        print(f"User name {new_user} is available, You may proceed")
+
+
+# Q 5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, such 
+# as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+# •	Store the numbers 1 through 9 in a list.
+# •	Loop through the list.
+# •	Use an if-elif-else chain inside the loop to print the proper ordinal end
+# ing for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 
+# 7th 8th 9th", and each result should be on a separate line.
+
+
+numbers = list(range(1,10))
+
+for number in numbers:
+    if number == 1:
+        print('1st')
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    elif number == 4:
+        print("4th")
+    elif number == 5:
+        print("5th")
+    elif number == 6:
+        print("6th")
+    elif number == 7:
+        print("7th")
+    elif number == 8:
+        print("8th")
+    elif number == 9:
+        print("9th")
+
+
+
+# Q 5-12. Styling if statements: Review the programs you wrote in this chapter, and 
+# make sure you styled your conditional tests appropriately.
+
+
+# Ans: i got through the each and evry lines of the codes and where it needed to be styling i did that 
+
+
+# 5-13. Your Ideas: At this point, you’re a more capable programmer than you 
+# were when you started this book. Now that you have a better sense of how 
+# real-world situations are modeled in programs, you might be thinking of some 
+# problems you could solve with your own programs. Record any new ideas you 
+# have about problems you might want to solve as your programming skills con
+# tinue to improve. Consider games you might want to write, data sets you might 
+# want to explore, and web applications you’d like to create.
+
+
+# Honestly at this point i am not able to handle the datasets but if i want to analyze it manually that is something which I'm able to do it
