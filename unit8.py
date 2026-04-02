a299c1638e87ca4007a00831f57b26a7896cfdf5
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-04-02 23:14:20 +0530

    in b/w of unit 8 {functions}

--- unit8.py
+++ unit8.py
@@ -179,3 +179,109 @@ def build_person(first_name, last_name, age= None):
 
 musician = build_person('jimmi', 'hendrix', age=35)
 print(musician)
+
+
+# def get_formated(first_name, last_name):
+#     """Returning full name formated"""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# while True:
+#     print("Please tell me your full name")
+#     print("Enter 'q' to quit from the program")
+#     f_name = input("Enter first name")
+#     if (f_name == 'q'):
+#         break;
+#     l_name = input("Enter last name")
+#     if (l_name == 'q'):
+#         break;
+#     else:
+#         f_name = input("Enter your first name ")
+#         l_name = input("Enter your last name ")
+
+#         formatted_name = get_formated(f_name, l_name)
+#         print(f"Hello {formatted_name}")
+
+
+
+# Q 8-6. City Names: Write a function called city_country() that takes in the name 
+# of a city and its country. The function should return a string formatted like this:
+# "Santiago, Chile"
+# Call your function with at least three city-country pairs, and print the 
+# values that are returned.
+
+
+def city_country(city, country):
+    return f"{city}, {country}"
+
+print (city_country("Santiago", "chile"))
+print(city_country("Hyderabad", "India"))
+print(city_country("Banglore", "india"))
+
+
+# Q  08-7. Album: Write a function called make_album() that builds a dictionary 
+# describing a music album. The function should take in an artist name and an 
+# album title, and it should return a dictionary containing these two pieces of 
+# information. Use the function to make three dictionaries representing different 
+# albums. Print each return value to show that the dictionaries are storing the 
+# album information correctly.
+# Use None to add an optional parameter to make_album() that allows you to 
+# store the number of songs on an album. If the calling line includes a value for 
+# the number of songs, add that value to the album’s dictionary. Make at least 
+# one new function call that includes the number of songs on an album.
+
+
+#ANS:- 
+def make_album(artist, title, songs=None):
+    """Build a dictionary describing a music album"""
+    album = {
+        "artist": artist,
+        "title": title
+    }
+    if songs:
+        album["songs"] = songs
+    return album
+
+album1 = make_album("Ed sheerean", "Divide")
+album2 =  make_album("Taylor swift", '1989')
+album3 = make_album("Adele", '25')
+
+
+
+print(album1)
+print(album2)
+print(album3)
+
+album4 = make_album("coldplay", "parachutes", songs=10)
+print(album4)
+
+
+# Q 8-8. User Albums: Start with your program from Exercise 8-7. Write a while 
+# loop that allows users to enter an album’s artist and title. Once you have that 
+# information, call make_album() with the user’s input and print the dictionary 
+# that’s created. Be sure to include a quit value in the while loop.
+
+
+def make_album(artist, title, songs=None):
+    """Build a dictionary describing a music album."""
+    album = {"artist": artist, "title": title}
+    if songs:
+        album["songs"] = songs
+    return album
+
+while True:
+    print("\nEnter album details (or type 'quit' to stop):")
+    artist = input("Artist name: ")
+    if artist.lower() == 'quit':
+        break
+
+    title = input("Album title: ")
+    if title.lower() == 'quit':
+        break
+
+    songs = input("Number of songs (optional, press Enter to skip): ")
+    if songs.lower() == 'quit':
+        break
+
+    album = make_album(artist, title, int(songs) if songs else None)
+    print("Album dictionary created:", album)
