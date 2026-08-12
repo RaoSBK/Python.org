6c629755b43ad8bbd0839ce0819c9587a38af7be
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-08-12 21:09:58 +0530

    classes

--- Revise1to8.py
+++ Revise1to8.py
@@ -618,3 +618,55 @@ my_car.brake(20)
 
 # Honk
 my_car.honk()
+
+
+
+
+
+
+# Question 13 — Bank Account Class
+# Create a class named:
+# BankAccount
+# Requirements
+# The class should have these attributes:
+# account_holder
+# account_number
+# balance
+
+
+
+class BankAccount:
+    def __init__(self, account_holder, account_number, balance=0.0):
+        self.account_holder = account_holder
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited {amount}. New balance: {self.balance}")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient funds.")
+
+    def display_info(self):
+        print(f"Account Holder: {self.account_holder}")
+        print(f"Account Number: {self.account_number}")
+        print(f"Balance: {self.balance}")
+
+
+
+# Create an account
+account1 = BankAccount("Suraj Bhan", "123456789", 5000)
+
+# Perform operations
+account1.display_info()
+account1.deposit(1500)
+account1.withdraw(2000)
+account1.display_info()
